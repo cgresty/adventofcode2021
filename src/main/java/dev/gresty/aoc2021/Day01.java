1bf04428578b15d77e315a1d437fc516d703e145
Chris Gresty <chris.gresty@gmail.com>
2021-12-01 23:00:43 +0000

    Day 1 - refined

--- src/main/java/dev/gresty/aoc2021/Day01.java
+++ src/main/java/dev/gresty/aoc2021/Day01.java
@@ -4,7 +4,6 @@ import java.util.stream.IntStream;
 
 import static dev.gresty.aoc2021.Utils.runInt;
 import static java.util.stream.IntStream.range;
-import static java.util.stream.IntStream.rangeClosed;
 
 public class Day01 {
 
@@ -14,34 +13,34 @@ public class Day01 {
     }
 
     static int day01a(IntStream input) {
-        WindowedCounter counter = new WindowedCounter(1);
+        Counter counter = new Counter(1);
         input.forEach(counter::submit);
         return counter.count;
     }
 
     static int day01b(IntStream input) {
-        WindowedCounter counter = new WindowedCounter(3);
+        Counter counter = new Counter(3);
         input.forEach(counter::submit);
         return counter.count;
     }
 
-    private static class WindowedCounter {
+    private static class Counter {
         final int size;
         final int[] w;
         int ok;
         int count = 0;
 
-        WindowedCounter(int windowSize) {
+        Counter(int windowSize) {
             size = windowSize;
             w = new int[size + 1];
             ok = -size;
         }
 
+        // To compare A+B+C to B+C+D, only need to compare A to D.
         void submit(int value) {
-            rangeClosed(1, size).forEach(i -> w[i] += value);
-            if (++ok > 0 && w[1] > w[0]) count++;
+            w[size] = value;
+            if (++ok > 0 && w[size] > w[0]) count++;
             range(0, size).forEach(i -> w[i] = w[i + 1]);
-            w[size] = 0;
         }
     }
 }
