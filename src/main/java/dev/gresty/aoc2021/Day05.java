e4ab7b9ab50bd3ec24c47a5e776937606f7b7eaf
Chris Gresty <chris.gresty@gmail.com>
2021-12-06 13:56:30 +0000

    Day 5 reworked

--- src/main/java/dev/gresty/aoc2021/Day05.java
+++ src/main/java/dev/gresty/aoc2021/Day05.java
@@ -1,21 +1,11 @@
 package dev.gresty.aoc2021;
 
-import dev.gresty.aoc2021.Utils.Point2;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import dev.gresty.aoc2021.Utils.IntPair;
+import dev.gresty.aoc2021.Utils.Node;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.function.BiPredicate;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static dev.gresty.aoc2021.Utils.withStrings;
-import static java.lang.Math.abs;
-import static java.lang.Math.signum;
 
 public class Day05 {
 
@@ -25,120 +15,40 @@ public class Day05 {
     }
 
     public static int part1(Stream<String> input) {
-        Set<Point2> intersections = new HashSet<>();
-        VentLine[] lines = input.map(i -> Day05.ventLine(i, false))
-                .filter(Objects::nonNull)
-                .toArray(VentLine[]::new);
-        for (int i = 0; i < lines.length - 1; i++) {
-            for (int j = i + 1; j < lines.length; j++) {
-                intersections.addAll(lines[i].intersection(lines[j]));
-            }
-        }
-        return intersections.size();
+        VentMapper mapper = new VentMapper(false);
+        input.forEach(mapper::submit);
+        return mapper.dangerous;
     }
 
     public static int part2(Stream<String> input) {
-        Set<Point2> intersections = new HashSet<>();
-        VentLine[] lines = input.map(i -> Day05.ventLine(i, true))
-                .filter(Objects::nonNull)
-                .toArray(VentLine[]::new);
-        for (int i = 0; i < lines.length - 1; i++) {
-            for (int j = i + 1; j < lines.length; j++) {
-                intersections.addAll(lines[i].intersection(lines[j]));
+        VentMapper mapper = new VentMapper(true);
+        input.forEach(mapper::submit);
+        return mapper.dangerous;
+    }
+
+    static class VentMapper {
+
+        Node ventMap = new Node();
+        int dangerous;
+        boolean includeDiagonals;
+
+        VentMapper(boolean includeDiagonals) {
+            this.includeDiagonals = includeDiagonals;
+        }
+
+        void submit(String input) {
+            String[] ends = input.split(" ");
+            IntPair start = IntPair.of(ends[0]);
+            IntPair end = IntPair.of(ends[2]);
+            IntPair d = end.subtract(start).signum();
+            if (d.x != 0 && d.y != 0 && !includeDiagonals) return;
+            IntPair i = start.subtract(d);
+            while (!i.equals(end)) {
+                i = i.add(d);
+                if (ventMap.store(i.toArray()) == 2) {
+                    dangerous++;
+                }
             }
         }
-        return intersections.size();
     }
-
-    static VentLine ventLine(String input, boolean includeDiagonals) {
-        String[] ends = input.split(" ");
-        String[] startStr = ends[0].split(",");
-        String[] endStr = ends[2].split(",");
-        Point2 start = new Point2(Integer.parseInt(startStr[0]), Integer.parseInt(startStr[1]));
-        Point2 end = new Point2(Integer.parseInt(endStr[0]), Integer.parseInt(endStr[1]));
-        if (start.x == end.x) {
-            if (start.y < end.y)
-                return new VerticalVentLine(start, end);
-            else
-                return new VerticalVentLine(end, start);
-        } else if (start.y == end.y) {
-            if (start.x < end.x)
-                return new HorizontalVentLine(start, end);
-            else
-                return new HorizontalVentLine(end, start);
-        } else if (includeDiagonals) {
-            if (start.y < end.y)
-                return new DiagonalVentLine(start, end);
-            else
-                return new DiagonalVentLine(end, start);
-        }
-        return null;
-    }
-
-    interface VentLine {
-        List<Point2> intersection(VentLine other);
-        List<Point2> satisfies(BiPredicate<Integer, Integer> predicate);
-    }
-
-    @RequiredArgsConstructor
-    @ToString
-    static class HorizontalVentLine implements VentLine {
-        final Point2 from;
-        final Point2 to;
-
-        @Override
-        public List<Point2> intersection(VentLine other) {
-            return other.satisfies((x,y) -> y == from.y && x >= from.x && x <= to.x);
-        }
-
-        @Override
-        public List<Point2> satisfies(BiPredicate<Integer, Integer> predicate) {
-            return IntStream.rangeClosed(from.x, to.x)
-                    .filter(x -> predicate.test(x, from.y))
-                    .mapToObj(x -> new Point2(x, from.y))
-                    .collect(Collectors.toList());
-        }
-    }
-
-    @RequiredArgsConstructor
-    @ToString
-    static class VerticalVentLine implements VentLine {
-        final Point2 from;
-        final Point2 to;
-
-        @Override
-        public List<Point2> intersection(VentLine other) {
-            return other.satisfies((x,y) -> x == from.x && y >= from.y && y <= to.y);
-        }
-
-        @Override
-        public List<Point2> satisfies(BiPredicate<Integer, Integer> predicate) {
-            return IntStream.rangeClosed(from.y, to.y)
-                    .filter(y -> predicate.test(from.x, y))
-                    .mapToObj(y -> new Point2(from.x, y))
-                    .collect(Collectors.toList());
-        }
-    }
-
-    @RequiredArgsConstructor
-    @ToString
-    static class DiagonalVentLine implements VentLine {
-        final Point2 from;
-        final Point2 to;
-
-        @Override
-        public List<Point2> intersection(VentLine other) {
-            return other.satisfies((x,y) -> signum(x - from.x) != signum(x - to.x) && y >= from.y && y <= to.y && abs(x - from.x) == abs(y - from.y));
-        }
-
-        @Override
-        public List<Point2> satisfies(BiPredicate<Integer, Integer> predicate) {
-            int dx = (int) Math.signum(to.x - from.x);
-            return IntStream.rangeClosed(0, to.y - from.y)
-                    .mapToObj(d -> new Point2(from.x + d * dx, from.y + d))
-                    .filter(p -> predicate.test(p.x, p.y))
-                    .collect(Collectors.toList());
-        }
-    }
-
 }
--- src/main/java/dev/gresty/aoc2021/Utils.java
+++ src/main/java/dev/gresty/aoc2021/Utils.java
@@ -8,9 +8,13 @@ import lombok.ToString;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.lang.Integer.parseInt;
 
 public class Utils {
 
@@ -35,8 +39,49 @@ public class Utils {
     @RequiredArgsConstructor
     @EqualsAndHashCode
     @ToString
-    static class Point2 {
+    static class IntPair {
         final int x;
         final int y;
+
+        IntPair add(IntPair other) {
+            return new IntPair(x + other.x, y + other.y);
+        }
+
+        IntPair subtract(IntPair other) {
+            return new IntPair(x - other.x, y - other.y);
+        }
+
+        IntPair signum() {
+            return new IntPair(Integer.signum(x), Integer.signum(y));
+        }
+
+        int[] toArray() {
+            return new int[] {x, y};
+        }
+
+        static IntPair of(String asString) {
+            String[] values = asString.split(",");
+            return new IntPair(parseInt(values[0]), parseInt(values[1]));
+        }
     }
+
+    static class Node {
+        Map<Integer, Node> children = new HashMap<>();
+        int count;
+
+        int store(int[] value) {
+            return store(value, 0);
+        }
+
+        private int store(int[] value, int offset) {
+            count++;
+            if (value.length > offset) {
+                int index = value[offset];
+                Node child = children.computeIfAbsent(index, i -> new Node());
+                return child.store(value, offset + 1);
+            }
+            return count;
+        }
+    }
+
 }
