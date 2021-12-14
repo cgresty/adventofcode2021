fea3d41a95e27ac8e26f27cf9ad3df89f147bbe7
Chris Gresty <chris.gresty@gmail.com>
2021-12-14 00:42:27 +0000

    Day 13 speed bump (in-place folding)

--- src/main/java/dev/gresty/aoc2021/Day13.java
+++ src/main/java/dev/gresty/aoc2021/Day13.java
@@ -1,11 +1,9 @@
 package dev.gresty.aoc2021;
 
 import dev.gresty.aoc2021.Utils.IntPair;
-import dev.gresty.aoc2021.Utils.Pair;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -50,7 +48,7 @@ public class Day13 {
     }
 
     static class Folder {
-        Set<IntPair> dots = new HashSet<>();
+        List<int[]> dots = new ArrayList<>();
         List<Instruction> instructions = new ArrayList<>();
 
         Folder(Stream<String> input) {
@@ -59,44 +57,37 @@ public class Day13 {
                         if (line.startsWith("fold")) {
                             instructions.add(Instruction.from(line));
                         } else {
-                            dots.add(IntPair.of(line));
+                            dots.add(IntPair.of(line).toArray());
                         }
                     });
         }
 
-        Set<IntPair> foldAll() {
-            Set<IntPair> folded = dots;
-            for (Instruction instr : instructions) {
-                folded = fold(folded, instr);
-            }
-            return folded;
-        }
-
         Set<IntPair> foldFirst() {
-            return fold(dots, instructions.get(0));
+            List<int[]> dots = this.dots;
+            instructions.get(0).fold(dots);
+            return dedup(dots);
         }
 
-        Set<IntPair> fold(Set<IntPair> dots, Pair<Character, Integer> instr) {
-            return instr.a == 'x' ? foldOnX(dots, instr.b) : foldOnY(dots, instr.b);
+        Set<IntPair> foldAll() {
+            List<int[]> dots = this.dots;
+            for (Instruction instr : instructions) {
+                instr.fold(dots);
+            }
+            return dedup(dots);
         }
 
-        Set<IntPair> foldOnX(Set<IntPair> dots, int fold) {
-            return dots.stream()
-                    .map(d -> d.x < fold ? d : IntPair.xy(2 * fold - d.x, d.y))
-                    .collect(toSet());
-        }
-
-        Set<IntPair> foldOnY(Set<IntPair> dots, int fold) {
-            return dots.stream()
-                    .map(d -> d.y < fold ? d : IntPair.xy(d.x, 2 * fold - d.y))
-                    .collect(toSet());
+        Set<IntPair> dedup(List<int[]> dots) {
+            return dots.stream().map(dot -> IntPair.xy(dot[0], dot[1])).collect(toSet());
         }
     }
 
-    static class Instruction extends Pair<Character, Integer> {
+    static class Instruction {
+        int axis;
+        int value;
 
         public Instruction(Character character, Integer integer) {
-            super(character, integer);
+            axis = character - 'x';
+            value = integer;
         }
 
         static Instruction from(String line) {
@@ -104,6 +95,14 @@ public class Day13 {
             int equals = line.lastIndexOf('=');
             return new Instruction(line.charAt(lastSpace + 1), parseInt(line.substring(equals + 1)));
         }
+
+        void fold(List<int[]> dots) {
+            for (int[] dot : dots) {
+                if (dot[axis] > value)
+                    dot[axis] = 2 * value - dot[axis];
+            }
+        }
+
     }
 
 
